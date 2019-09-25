aca9b01542c934198f7d6f288d6f04d60c8f9598
nconrad <1194246+nconrad@users.noreply.github.com>
2019-09-25 15:17:24 -0500

    testing CI

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   launch: {
     dumpio: true,
-    headless: false, // process.env.HEADLESS !== 'false',
+    headless: process.env.HEADLESS !== 'false',
     // slowMo: 500
   },
   browserContext: 'default',
