b159d11a101a481f6e1b232f691f1fcdc902fa6e
nconrad <1194246+nconrad@users.noreply.github.com>
2019-09-26 14:48:18 -0500

    config

--- tests/genome-tabs/subsystems.test.js
+++ tests/genome-tabs/subsystems.test.js
@@ -9,8 +9,6 @@ describe('Subsystems tab', () => {
   beforeAll(async () => {
     const view = `${url}/view/GenomeList/?in(genome_id,(106648.10,1144674.3,1785128.3))#view_tab=pathways`
     await page.goto(view, {waitUntil: 'networkidle0', timeout})
-    const performanceMetrics = await page._client.send('Performance.getMetrics');
-    console.log(page.metrics());
   })
 
   it('should say have the query at the top of the page', async () => {
@@ -23,8 +21,8 @@ describe('Subsystems tab', () => {
     const pieChart = await page.evaluate(() =>
       document.querySelector('#subsystemspiechart svg')
     )
-    console.log('pieChart', pieChart)
-    return pieChart == null
+
+    return pieChart != null
   })
 
 })
