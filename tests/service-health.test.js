c8e9e54f9c166123d64d555cd0fa306c19f0c9ed
nconrad <1194246+nconrad@users.noreply.github.com>
2019-09-30 23:01:24 -0500

    add service health tests; add network idle for protein fams

---
+++ tests/service-health.test.js
@@ -0,0 +1,80 @@
+
+const axios = require('axios')
+
+const auth = 'https://user.patricbrc.org/'
+const dataAPI = 'https://patricbrc.org/api/genome/?http_accept=application/solr+json'
+const ws = 'https://p3.theseed.org/services/Workspace/ping'
+const shock = 'https://p3.theseed.org/services/shock_api/node'
+const appService = 'https://p3.theseed.org/services/app_service/ping'
+const minHash = 'https://p3.theseed.org/services/minhash_service/ping'
+const homology = 'https://p3.theseed.org/services/homology_service/ping'
+
+
+describe('Auth', () => {
+
+  it('should return 200', async () => {
+    const res = await axios.get(auth)
+    expect(res.status).toBe(200)
+  })
+})
+
+
+describe('Data API', () => {
+
+  let res
+  beforeAll(async () => {
+    res = await axios.get(dataAPI)
+  })
+
+  it('should return 200', async () => {
+    expect(res.status).toBe(200)
+  })
+
+  it('should return 25 items', () => {
+    expect(res.data.response.docs.length).toBe(25)
+  })
+})
+
+
+describe('Workspace', () => {
+
+  it('should return 200', async () => {
+    const res = await axios.get(ws)
+    expect(res.status).toBe(200)
+  })
+})
+
+
+describe('App Service', () => {
+
+  it('should return 200', async () => {
+    const res = await axios.get(appService)
+    expect(res.status).toBe(200)
+  })
+})
+
+describe('Shock', () => {
+
+  it('should return 200', async () => {
+    const res = await axios.get(shock)
+    expect(res.status).toBe(200)
+  })
+})
+
+
+describe('Min Hash Service', () => {
+
+  it('should return 200', async () => {
+    const res = await axios.get(minHash)
+    expect(res.status).toBe(200)
+  })
+})
+
+
+describe('Homology Service', () => {
+
+  it('should return 200', async () => {
+    const res = await axios.get(homology)
+    expect(res.status).toBe(200)
+  })
+})
--- tests/ui/genome-tabs/pfams.test.js
+++ tests/ui/genome-tabs/pfams.test.js
@@ -1,4 +1,4 @@
-
+const sleep = require('util').promisify(setTimeout)
 const url = 'https://patricbrc.org'
 
 
@@ -6,7 +6,7 @@ describe('Brucella pinnipedialis B2/94 protein families tab', () => {
 
   it('should list 200 proteins', async () => {
     const view = `${url}/view/Genome/520461.7#view_tab=proteinFamilies`
-    await page.goto(view)
+    await page.goto(view, {waitUntil: 'networkidle0'})
     const pageText = await page.evaluate(() =>
       document.querySelector('.dgrid-pagination .dgrid-status').innerText.slice(0, 7)
     )
